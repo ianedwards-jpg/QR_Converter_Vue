68c13523275225cdcd5887437991e22b66b07769
Ian Edwards <57515741+ianedwards-jpg@users.noreply.github.com>
2021-08-15 14:32:06 -0400

    First Update
    Header Component Created
    HelloWorld Component Deleted
    App.Vue:
    New container div added to template, border implemented
    New Styling Added
    Links to old components removed from template/imports, old styling removed

---
+++ qr_coverter/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <h1>QR Converter</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style scoped>
+header {
+    display: flex,
+}
+</style>
